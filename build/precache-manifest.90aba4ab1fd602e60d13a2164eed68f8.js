56c0f38185dca1f63c2e4b4751b17df2b942d374
hyun chae <daniel.chae.a@gmail.com>
2020-04-25 17:19:52 +0700

    Add static build

---
+++ build/precache-manifest.90aba4ab1fd602e60d13a2164eed68f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7ebd5c51acf427c3d13695059a45a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae81f25589fe7a45972e",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "8e2bb66a0f51a8ceacc5",
+    "url": "/static/js/2.5921bab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5921bab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae81f25589fe7a45972e",
+    "url": "/static/js/main.0e2670da.chunk.js"
+  },
+  {
+    "revision": "8290b5cf2f3317cf7e20",
+    "url": "/static/js/runtime-main.2b702dae.js"
+  }
+]);
